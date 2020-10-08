3aa869771eee3eafc71f14f181ef35b864a3977b
Natalia Vytas <natyjazvytas@gmail.com>
2020-10-07 22:17:45 -0300

    tp3

---
+++ Practico 3/clock-js/index.js
@@ -0,0 +1,68 @@
+let canvas = null;
+let ctx = null;
+
+let centerX = 0;
+let centerY = 0;
+let lengthArrowHours;
+let lengthArrowMinutes;
+let lengthArrowSeconds;
+
+window.addEventListener('load', onload);
+
+function onload() {
+    canvas = document.querySelector('#canvas');
+    ctx = canvas.getContext("2d");
+
+    centerX = canvas.width / 2;
+    centerY = canvas.height / 2;
+
+    radius = (canvas.width / 2) * 0.8;
+
+    lengthArrowHours = radius * 0.65;
+    lengthArrowMinutes = radius * 0.75;
+    lengthArrowSeconds = radius * 0.85
+
+    setInterval(redraw, 1000);
+}
+
+function redraw() {
+    ctx.fillStyle = '#CCCCCC';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 8;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+
+    let now = new Date();
+
+    ctx.strokeStyle = '#000000';
+
+    ctx.lineWidth = 6;
+    let angle = (now.getHours() / 60) * 2 * Math.PI - Math.PI / 2;
+    let px = centerX + Math.cos(angle) * lengthArrowHours;
+    let py = centerY + Math.sin(angle) * lengthArrowHours;
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(px, py);
+    ctx.stroke();
+
+    ctx.lineWidth = 4;
+    angle = (now.getMinutes() / 60) * 2 * Math.PI - Math.PI / 2;
+    px = centerX + Math.cos(angle) * lengthArrowMinutes;
+    py = centerY + Math.sin(angle) * lengthArrowMinutes;
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(px, py);
+    ctx.stroke();
+
+    ctx.lineWidth = 2;
+    angle = (now.getSeconds() / 60) * 2 * Math.PI - Math.PI / 2;
+    px = centerX + Math.cos(angle) * lengthArrowSeconds;
+    py = centerY + Math.sin(angle) * lengthArrowSeconds;
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(px, py);
+    ctx.stroke();
+
+
+}
